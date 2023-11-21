a00936b31a104360218504da432c2580ae5df07e
mysoyul <vega2k_@naver.com>
2023-11-21 17:55:07 +0900

    App.vue update

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
 </script>
 
 <template>
-  <div>
-    
+  <div class="app-container" :class="authStore.user && 'bg-light'">
+    <Nav />
+    <Alert />
+    <div class="container pt-4 pb-4">
+      <router-view />
+    </div>
   </div>
 </template>
 
